20629b9f972bf4c56864bef54a107179aa88a80a
Ortholite <ortholite-k@yandex.ru>
2021-04-09 15:54:55 +0300

    Add forked third party code.

---
+++ ThirdPartyFork/MDXBuilderLibrary/mdx/axisitems/Functions/Members.cs
@@ -0,0 +1,12 @@
+﻿using MDXBuilderLibrary.mdx.interfaces;
+
+namespace MDXBuilderLibrary.mdx.axisitems.Functions
+{
+    public class Members
+        : SuffixFunction<Members>
+    {
+        public Members(IMDXAxisItem item)
+            : base(item)
+        { }
+    }
+}
