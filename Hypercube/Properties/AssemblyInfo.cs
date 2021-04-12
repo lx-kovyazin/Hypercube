87935c8dcda79f10c92c59ff099285705451add6
Ortholite <ortholite-k@yandex.ru>
2021-04-12 17:01:55 +0300

    Added description.

--- Hypercube/Properties/AssemblyInfo.cs
+++ Hypercube/Properties/AssemblyInfo.cs
@@ -7,7 +7,11 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyProduct("Ковязин Александр Сергеевич")]
 [assembly: AssemblyCompany("Группа ИВТм-2302-02-00, кафедра ЭВМ, ВятГУ")]
 [assembly: AssemblyCopyright("2021, город Киров")]
-[assembly: AssemblyDescription("Описание \n Описание")]
+[assembly: AssemblyDescription(
+    @"Программа, предназначенная для обозревания гиперкубов SQL Server Analysis Services.
+Программа позволяет строить запросы к гиперкубу на языке MDX и выводить результаты в виде сводной таблицы данных.
+    "
+)]
 
 [assembly: ComVisible(false)]
 [assembly: Guid("8492ec4f-6f47-463f-a60d-b36ae9709642")]
