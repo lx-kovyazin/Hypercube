39b8293f01838a3fbd1792f1099dc3fbbd226fb5
Ortholite <ortholite-k@yandex.ru>
2021-04-26 20:10:32 +0300

    Checked out models.

---
+++ HypercubeClient/Model/Member.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AnalysisServices.AdomdClient;
+using AdomdMember = Microsoft.AnalysisServices.AdomdClient.Member;
+
+namespace Hypercube.Client.Model
+{
+    public class Member
+        : IMetaModel
+    {
+        protected readonly AdomdMember member;
+
+        public Member(AdomdMember member)
+            => this.member = member ?? throw new ArgumentNullException(nameof(member));
+
+        public string FriendlyName => member.Caption;
+        public string UniqueName => member.UniqueName;
+    }
+}
