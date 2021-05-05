ad55711e358be0fdceb98c0b8fabcb4873c47fc7
Ortholite <ortholite-k@yandex.ru>
2021-05-05 14:59:51 +0300

    ~ Finished the filter feature for 2 operators.

--- Hypercube/Control/Filter/FilterListViewOperatorSubItem.cs
+++ Hypercube/Control/Filter/FilterListViewOperatorSubItem.cs
@@ -1,21 +1,40 @@
-﻿using MaterialSkin.Controls;
+﻿using System.Collections.Generic;
+using System.Linq;
+using MaterialSkin.Controls;
 
 namespace Hypercube.Control.Filter
 {
     internal class FilterListViewOperatorSubItem
         : FilterListViewSubItem
     {
+        internal enum Operator
+        {
+            Equals,
+            NotEquals,
+
+        }
+
+        private readonly Dictionary<Operator, string> operatorMap
+            = new Dictionary<Operator, string>
+            {
+                [Operator.Equals]    = "=",
+                [Operator.NotEquals] = "≠",
+            };
+
         private readonly MaterialComboBox operatorComboBox;
 
         public FilterListViewOperatorSubItem(FilterListViewItem owner)
             : base(owner)
         {
             operatorComboBox = new MaterialComboBox();
-            operatorComboBox.Items.AddRange(new string[] { "=", "≠" });
+            operatorComboBox.Items.AddRange(operatorMap.Values.ToArray());
             operatorComboBox.AutoResize = false;
             operatorComboBox.UseTallSize = false;
             Container = operatorComboBox;
         }
+
+        public Operator Current
+            => operatorMap.FirstOrDefault(x => x.Value == Text).Key;
 
         protected override void SetContainerText()
             => operatorComboBox.Hint = Text;
--- Hypercube/Control/QueryConstructor.cs
+++ Hypercube/Control/QueryConstructor.cs
@@ -1,15 +1,17 @@
 ﻿using Hypercube.Client;
+using Hypercube.Control.Filter;
 using MdxBuilder;
 using MdxBuilder.Builder;
 using MdxBuilder.Entity;
 using MdxBuilder.Entity.Function.Set;
-using MdxBuilder.Entity.Operator.Set;
+using MdxBuilder.Entity.Operator.Unary;
 using MdxBuilder.Utils;
 using Microsoft.AnalysisServices.AdomdClient;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -17,6 +19,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using Crossjoin = MdxBuilder.Entity.Operator.Set.Crossjoin;
 using HypercubeClient = Hypercube.Client.Client;
+using Set = MdxBuilder.Entity.Set;
 
 namespace Hypercube.Control
 {
@@ -25,15 +28,62 @@ namespace Hypercube.Control
         public event Action<CellSet> DataCollected;
 
         public QueryConstructor()
+            => InitializeComponent();
+
+        private static UniqueEntity TransformFilterItem(FilterListViewItem item)
         {
-            InitializeComponent();
+            var filterListSubItems = item.SubItems.Cast<FilterListViewSubItem>().ToArray();
+            var hsi = filterListSubItems[0] as FilterListViewHierarchySubItem;
+            var osi = filterListSubItems[1] as FilterListViewOperatorSubItem;
+            var vsi = filterListSubItems[2] as FilterListViewValueSubItem;
+            Debug.Print($"On {hsi.Hierarchy} perform {osi.Current} by {vsi.Text}");
+
+            var msb = new SetBuilder();
+            vsi.SelectedMembers.ForEach(
+                m => msb.Add(tb => tb.Add(new UniqueEntity(m.UniqueName)))
+            );
+
+            UniqueEntity op;
+            switch (osi.Current)
+            {
+                case FilterListViewOperatorSubItem.Operator.Equals:
+                    op = new UniqueEntity(msb.Build().Body);
+                    break;
+
+                case FilterListViewOperatorSubItem.Operator.NotEquals:
+                    op = Negative.Do(new UniqueEntity(msb.Build().Body).AsOp());
+                    break;
+
+                default:
+                    throw new Exception($"Unexpected filter operator: {osi.Current}.");
+            }
+
+            return op;
         }
+
+        private SubCube CreateSubcubeRecursively(
+            IEnumerator<FilterListViewItem> items
+            )
+        {
+            var scb = new SubCubeBuilder();
+            if (!items.MoveNext())
+                return scb.Set(new Cube(HypercubeClient.Instance.MetaInfo.CurrentCube.UniqueName)).Build();
+
+            var op = TransformFilterItem(items.Current);
+            return scb.Set(qb => qb
+                .Select(seb => seb
+                    .Add(aab => aab
+                        .SetDimension(AliasedAxis.Alias.Columns)
+                        .Set(sb => sb.Add(tb => tb.Add(op)))))
+                .From(feb => feb.Set(CreateSubcubeRecursively(items)))
+                ).Build();
+        }
+
 
         private MdxCommandProvider PrepareCommand()
         {
             var dimensionMetaListViewItems = constructorComponent.dimensionMetaListView.fixedListView.Items.Cast<FixedMetaListViewItem>();
             var measureMetaListViewItems = constructorComponent.measureMetaListView.fixedListView.Items.Cast<FixedMetaListViewItem>();
-            var cube = new Cube(HypercubeClient.Instance.MetaInfo.CurrentCube.UniqueName);
 
             List<UniqueEntity> listConverter
                 (IEnumerable<FixedMetaListViewItem> list)
@@ -45,6 +95,14 @@ namespace Hypercube.Control
 
             var dimensions = listConverter(dimensionMetaListViewItems)
                 .Select(item => AllMembers.Do(item)).AsOp();
+
+            var filterListItems
+                = constructorComponent.filterComponent
+                                      .filterListView
+                                      .Items
+                                      .Cast<FilterListViewItem>();
+
+            var sc = CreateSubcubeRecursively(filterListItems.GetEnumerator());
 
             var query
                 = Mdx.Create()
@@ -59,39 +117,9 @@ namespace Hypercube.Control
                                  Crossjoin.Do(dimensions)
                         ))))
                      )
-                     .From(feb => feb.Set(sc => sc.Set(cube)))
+                     .From(feb => feb.Set(sc))
                      .Build()
                      ;
-
-            //var filterListItems
-            //    = constructorComponent.filterComponent
-            //                          .filterListView
-            //                          .Items
-            //                          .Cast<FilterListViewItem>();
-            //foreach (var item in filterListItems)
-            //{
-            //    var filterListSubItems = item.SubItems.Cast<FilterListViewSubItem>();
-            //    foreach (var subItem in filterListSubItems)
-            //    {
-            //        switch (subItem)
-            //        {
-            //            case FilterListViewHierarchySubItem hsi:
-            //                hsi.Hierarchy;
-            //                break;
-
-            //            case FilterListViewOperatorSubItem osi:
-            //                osi.Text;
-            //                break;
-
-            //            case FilterListViewValueSubItem vsi:
-            //                vsi.SelectedMembers;
-            //                break;
-
-            //            default:
-            //                break;
-            //        }
-            //    }
-            //}
 
             return new MdxCommandProvider(query);
         }
