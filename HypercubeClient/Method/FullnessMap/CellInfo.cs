c85cace27e357d2d601569222ee482eadb46f4ca
wekviem <wekviem@yandex.ru>
2021-05-06 23:00:36 +0300

    .

---
+++ HypercubeClient/Method/FullnessMap/CellInfo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Hierarchy = Microsoft.AnalysisServices.AdomdClient.OlapInfoHierarchyCollection;
+
+namespace Hypercube.Client.Method.FullnessMap
+{
+    class CellInfo
+    {
+        public Dictionary<Hierarchy, string> Info
+        {
+            private set; get;
+        }
+
+        public decimal Factor 
+        { 
+            private set; get; 
+        }
+
+        public CellInfo(Hierarchy hierarchy)
+        {
+            Info = new Dictionary<Hierarchy, string>();
+            Factor = 0;
+        }
+    }
+}
---
+++ HypercubeClient/Method/FullnessMap/Map.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.AnalysisServices.AdomdClient;
+
+namespace Hypercube.Client.Method.FullnessMap
+{
+    class Map
+    {
+        public Map()
+        {
+        
+        }
+
+        public void LoadData(CellSet set)
+        {
+            
+        }
+    }
+}
